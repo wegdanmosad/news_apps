ffe2fb171e08fdd8d305e6de3af08a48012faf58
Wegdan Mosad <wegdan@example.com>
2025-08-28 23:29:51 +0200

    implement light theme

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -5,7 +5,41 @@ class AppTheme {
   static const Color white = Color(0xFFFFFFFF);
   static const Color grey = Color(0xFFA0A0A0);
 
-  static ThemeData lightTheme = ThemeData();
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: white,
+    appBarTheme: AppBarTheme(
+      backgroundColor: white,
+      foregroundColor: black,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+        color: black,
+      ),
+    ),
+    textTheme: TextTheme(
+      titleLarge: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w500,
+        color: black,
+      ),
+      labelLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: black,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+        color: black,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.bold,
+        color: black,
+      ),
+    ),
+  );
 
   static ThemeData darkTheme = ThemeData(
     scaffoldBackgroundColor: black,
